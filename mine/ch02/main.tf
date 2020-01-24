79d6b276e97e8a108d66f4cd7a6f707b046b7e5f
alex <unc741@gmail.com>
2020-01-24 04:49:25 -0500

    changed desired capacity to modify inline

--- mine/ch02/main.tf
+++ mine/ch02/main.tf
@@ -26,8 +26,9 @@ resource "aws_autoscaling_group" "example" {
   target_group_arns = [aws_lb_target_group.asg.arn]
   health_check_type = "ELB"
 
-  min_size = 2
-  max_size = 10
+  min_size          = 2
+  max_size          = 10
+  desired_capacity  = 3
 
   tag {
       key                   = "Name"
